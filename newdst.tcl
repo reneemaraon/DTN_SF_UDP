1ef6adfb6d133555b0d0ff894edb5464aa14afcd
Renee Maraon <reneemaraon@gmail.com>
2017-11-07 23:53:11 +0800

    200 seconds worth of ikot

--- newdst.tcl
+++ newdst.tcl
@@ -39,11 +39,30 @@ $ns_ at 89 "$node_(1) setdest 60 300 20"
 
 
 #14 s R
-$ns_ at 93 "$node_(1) setdest 340 300 20"
+$ns_ at 92 "$node_(1) setdest 340 300 20"
 #3 s U
-$ns_ at 107 "$node_(1) setdest 340 240 20"
+$ns_ at 106 "$node_(1) setdest 340 240 20"
 #14 s L
-$ns_ at 110 "$node_(1) setdest 60 240 20"
+$ns_ at 109 "$node_(1) setdest 60 240 20"
 # #3 s D GUMAGANA NAMAN PAG COMMENTED OUT KASO NAGSSISEGV ERROR SA NOHUP.OUT/ OKS SA NETANIM
-$ns_ at 124 "$node_(1) setdest 60 300 20"
+$ns_ at 123 "$node_(1) setdest 60 300 20"
 
+
+#14 s R
+$ns_ at 126 "$node_(1) setdest 340 300 20"
+#3 s U
+$ns_ at 140 "$node_(1) setdest 340 240 20"
+#14 s L
+$ns_ at 143 "$node_(1) setdest 60 240 20"
+# #3 s D GUMAGANA NAMAN PAG COMMENTED OUT KASO NAGSSISEGV ERROR SA NOHUP.OUT/ OKS SA NETANIM
+$ns_ at 157 "$node_(1) setdest 60 300 20"
+
+
+#14 s R
+$ns_ at 160 "$node_(1) setdest 340 300 20"
+#3 s U
+$ns_ at 174 "$node_(1) setdest 340 240 20"
+#14 s L
+$ns_ at 177 "$node_(1) setdest 60 240 20"
+# #3 s D GUMAGANA NAMAN PAG COMMENTED OUT KASO NAGSSISEGV ERROR SA NOHUP.OUT/ OKS SA NETANIM
+$ns_ at 191 "$node_(1) setdest 60 300 20"
