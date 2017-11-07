e81dfb15aab2d3660f60db069d69c68330504ec2
Renee Maraon <reneemaraon@gmail.com>
2017-11-08 00:27:32 +0800

    200s fixed ikot

--- newdst.tcl
+++ newdst.tcl
@@ -18,7 +18,7 @@ $ns_ at 17 "$node_(1) setdest 60 240 20"
 #3 s D
 $ns_ at 31 "$node_(1) setdest 60 300 20"
 
-#14 s R
+
 $ns_ at 34 "$node_(1) setdest 340 300 20"
 #3 s U
 $ns_ at 48 "$node_(1) setdest 340 240 20"
@@ -31,38 +31,38 @@ $ns_ at 65 "$node_(1) setdest 60 300 20"
 #14 s R
 $ns_ at 68 "$node_(1) setdest 340 300 20"
 #3 s U
-$ns_ at 72 "$node_(1) setdest 340 240 20"
+$ns_ at 82 "$node_(1) setdest 340 240 20"
 #14 s L
-$ns_ at 75 "$node_(1) setdest 60 240 20"
+$ns_ at 85 "$node_(1) setdest 60 240 20"
 # #3 s D GUMAGANA NAMAN PAG COMMENTED OUT KASO NAGSSISEGV ERROR SA NOHUP.OUT/ OKS SA NETANIM
-$ns_ at 89 "$node_(1) setdest 60 300 20"
+$ns_ at 99 "$node_(1) setdest 60 300 20"
 
 
 #14 s R
-$ns_ at 92 "$node_(1) setdest 340 300 20"
+$ns_ at 102 "$node_(1) setdest 340 300 20"
 #3 s U
-$ns_ at 106 "$node_(1) setdest 340 240 20"
+$ns_ at 116 "$node_(1) setdest 340 240 20"
 #14 s L
-$ns_ at 109 "$node_(1) setdest 60 240 20"
+$ns_ at 119 "$node_(1) setdest 60 240 20"
 # #3 s D GUMAGANA NAMAN PAG COMMENTED OUT KASO NAGSSISEGV ERROR SA NOHUP.OUT/ OKS SA NETANIM
-$ns_ at 123 "$node_(1) setdest 60 300 20"
+$ns_ at 133 "$node_(1) setdest 60 300 20"
 
 
 #14 s R
-$ns_ at 126 "$node_(1) setdest 340 300 20"
+$ns_ at 136 "$node_(1) setdest 340 300 20"
 #3 s U
-$ns_ at 140 "$node_(1) setdest 340 240 20"
+$ns_ at 150 "$node_(1) setdest 340 240 20"
 #14 s L
-$ns_ at 143 "$node_(1) setdest 60 240 20"
+$ns_ at 153 "$node_(1) setdest 60 240 20"
 # #3 s D GUMAGANA NAMAN PAG COMMENTED OUT KASO NAGSSISEGV ERROR SA NOHUP.OUT/ OKS SA NETANIM
-$ns_ at 157 "$node_(1) setdest 60 300 20"
+$ns_ at 167 "$node_(1) setdest 60 300 20"
 
 
 #14 s R
-$ns_ at 160 "$node_(1) setdest 340 300 20"
+$ns_ at 170 "$node_(1) setdest 340 300 20"
 #3 s U
-$ns_ at 174 "$node_(1) setdest 340 240 20"
+$ns_ at 184 "$node_(1) setdest 340 240 20"
 #14 s L
-$ns_ at 177 "$node_(1) setdest 60 240 20"
+$ns_ at 187 "$node_(1) setdest 60 240 20"
 # #3 s D GUMAGANA NAMAN PAG COMMENTED OUT KASO NAGSSISEGV ERROR SA NOHUP.OUT/ OKS SA NETANIM
-$ns_ at 191 "$node_(1) setdest 60 300 20"
+$ns_ at 201 "$node_(1) setdest 60 300 20"
